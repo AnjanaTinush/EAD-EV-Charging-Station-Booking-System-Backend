0b942865036922d23c1f790fa498d6ecf5fbc80f
Anjana Tinush <146495563+AnjanaTinush@users.noreply.github.com>
2025-09-25 15:36:39 +0530

    crea models

--- Ev-backend/Ev-backend/Models/Booking.cs
+++ Ev-backend/Ev-backend/Models/Booking.cs
@@ -1,6 +1,26 @@
-﻿namespace Ev_backend.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Ev_backend.Models
 {
     public class Booking
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("ownerNic")]
+        public string OwnerNIC { get; set; }   // Links to EVOwner.NIC
+
+        [BsonElement("stationId")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string StationId { get; set; }  // Links to Station.Id
+
+        [BsonElement("reservationTime")]
+        public DateTime ReservationTime { get; set; }
+
+        [BsonElement("status")]
+        public string Status { get; set; } = "Pending";
+        // Pending, Approved, Cancelled
     }
 }
